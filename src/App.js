58a74fa123a8278f5c5b021ad2953c7e96175422
qorals1121 <qorals1121@naver.com>
2023-05-05 17:43:45 +0900

    컴포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { Component } from 'react';
-import InterationSample from './InterationSample.js';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+// 랜덤 색상 생성
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+	state = {
+		color: '#000000'
+	}
+
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor()
+		});
+	}
+
 	render() {
 		return (
-			<InterationSample />
+			<div>
+				<button onClick={this.handleClick}>랜덤 색상</button>
+				<ErrorBoundary>
+					<LifeCycleSample color={this.state.color} />
+				</ErrorBoundary>
+			</div>
 		);
 	}
 }
